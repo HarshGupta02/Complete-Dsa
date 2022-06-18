3a6e47d6311b4b21b50fc3f0e4e29dc881456b75
Harsh Gupta <you@example.com>
2022-06-18 13:31:21 -0700

    Solved

---
+++ Strings/Longest Common Subsequence.cpp
@@ -0,0 +1,49 @@
+/// TIME = O(N * M) , SPACE = O(N * M)
+
+class Solution
+{
+    public:
+    //Function to find the length of longest common subsequence in two strings.
+    int lcs(int x, int y, string s1, string s2)
+    {
+        vector<vector<int>>dp(x + 1, vector<int>(y + 1,INT_MIN));
+        for(int i = 0; i <= x; i++){
+            for(int j = 0; j <= y; j++){
+                if(i == 0 or j == 0) dp[i][j] = 0;
+            }
+        }
+        for(int i = 1; i <= x; i++){
+            for(int j = 1; j <= y; j++){
+                if(s1[i - 1] == s2[j - 1])
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                else 
+                    dp[i][j] = max(dp[i - 1][j] , dp[i][j - 1]);
+            }
+        }
+        return dp[x][y];
+    }
+};
+
+
+//// TIME = O(N * M) , SPACE = O(M)
+
+class Solution
+{
+    public:
+    //Function to find the length of longest common subsequence in two strings.
+    int lcs(int x, int y, string s1, string s2)
+    {
+        vector<int>prev(y + 1,0) , curr(y + 1,0);
+        for(int i = 1; i <= x; i++){
+            curr[0] = 0;
+            for(int j = 1; j <= y; j++){
+                if(s1[i - 1] == s2[j - 1])
+                    curr[j] = 1 + prev[j - 1];
+                else 
+                    curr[j] = max(prev[j] , curr[j - 1]);
+            }
+            prev = curr;
+        }
+        return prev[y];
+    }
+};
