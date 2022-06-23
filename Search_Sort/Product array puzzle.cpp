03041072438d9d3e915a45ac4bbefe759c6631cb
Harsh Gupta <you@example.com>
2022-06-23 14:56:59 -0700

    Space Optimization done

--- Search_Sort/Product array puzzle.cpp
+++ Search_Sort/Product array puzzle.cpp
@@ -1,3 +1,5 @@
+/// METHOD 1: TIME = O(N) , SPACE = O(N)
+
 class Solution{
   public:
     using ll = long long int;
@@ -13,6 +15,34 @@ class Solution{
             if(i + 1 < n) p *= suffix[i + 1];
             ans.push_back(p);
         }
+        return ans;
+    }
+};
+
+/// METHOD 2 : TIME = O(N) , SPACE = O(1)
+
+// JUST STORED THE PREFIX IN ANSWER ARRAY AS DOES NOT COUNT IN SPACE COMPLEXITY AND ITERATED
+// FROM BACK AND CALCULATED THE SUFFIX PRODUCT USING LOOP AND FOUND THE ANS.
+
+class Solution{
+  public:
+    using ll = long long int;
+    
+    vector<long long int> productExceptSelf(vector<long long int>& nums, int n) {
+        vector<ll>ans;
+        ll prod = 1;
+        for(int i = 0 ; i < n ; i ++){
+            prod *= nums[i];
+            ans.push_back(prod);
+        }
+        prod = 1;
+        for(int i = n - 1; i >= 0; i--){
+            if(i - 1 >= 0){
+                ans[i] = ans[i - 1] * prod;
+                prod *= nums[i];
+            }
+        }
+        ans[0] = prod;
         return ans;
     }
 };
