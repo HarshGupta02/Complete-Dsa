6c44f7df74d52ff8ed56782786a94f81419eb48e
Harsh Gupta <you@example.com>
2022-06-27 12:43:00 -0700

    Solved

---
+++ Stack_And_Queue/insert an element at the Bottom of a Stack.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+const ll MOD = 1e9+7;
+
+#define debug(x) cout<<#x<<" = "<<x<<endl
+#define nline "\n"
+#define all(a) (a).begin(),(a).end()
+#define fast_cin() ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+
+stack<int>stk;
+
+void insert_at_bottom(int n){
+    if(stk.empty()){
+        stk.push(n);
+        return;
+    }
+    int x = stk.top();
+    stk.pop();
+    insert_at_bottom(n);
+    stk.push(x);
+}
+
+void solve(){
+    for(int i = 5; i >= 1; i --) stk.push(i);
+    insert_at_bottom(9);
+    while(!stk.empty()){
+        cout << stk.top() << " ";
+        stk.pop();
+    }
+    cout << endl;
+}
+
+int main() {
+    fast_cin();
+    ll test_cases = 1;
+    // cin >> test_cases;
+    for(ll it=1;it<=test_cases;it++) {
+        solve();
+    }
+    return 0;
+}
