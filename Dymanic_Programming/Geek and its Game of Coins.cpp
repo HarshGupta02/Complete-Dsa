67eb39b9873eca930365d34b943e63adfe8d47e4
Harsh Gupta <you@example.com>
2022-07-03 10:06:16 -0700

    Solved

--- Dymanic_Programming/Geek and its Game of Coins.cpp
+++ Dymanic_Programming/Geek and its Game of Coins.cpp
@@ -1,25 +1,25 @@
 class Solution{
+
 	public:
 	int findWinner(int N, int X, int Y)
 	{
-	    vector<vector<bool>>dp(3 ,vector<bool>(N + 1 , true));
-	    dp[1][0] = false; dp[2][0] = false;
-	    dp[1][1] = true; dp[2][1] = true;
+	    vector<vector<bool>>dp(2 ,vector<bool>(N + 1 , true));
+	    dp[0][0] = false; dp[1][0] = false;
+	    dp[0][1] = true; dp[1][1] = true;
 	    for(int i = 2; i <= N ; i ++){
-            
-	        if(i >= X) dp[1][i] = dp[1][i] and dp[2][i - X];
-	        if(i >= Y) dp[1][i] = dp[1][i] and dp[2][i - Y];
-	        if(i >= 1) dp[1][i] = dp[1][i] and dp[2][i - 1];
+	        if(i >= X) dp[0][i] = dp[0][i] and dp[1][i - X];
+	        if(i >= Y) dp[0][i] = dp[0][i] and dp[1][i - Y];
+	        if(i >= 1) dp[0][i] = dp[0][i] and dp[1][i - 1];
+	        
+	        dp[0][i] = !dp[0][i];
+	        
+	        if(i >= X) dp[1][i] = dp[1][i] and dp[0][i - X];
+	        if(i >= Y) dp[1][i] = dp[1][i] and dp[0][i - Y];
+	        if(i >= 1) dp[1][i] = dp[1][i] and dp[0][i - 1];
 	        
 	        dp[1][i] = !dp[1][i];
-	        
-	        if(i >= X) dp[2][i] = dp[2][i] and dp[1][i - X];
-	        if(i >= Y) dp[2][i] = dp[2][i] and dp[1][i - Y];
-	        if(i >= 1) dp[2][i] = dp[2][i] and dp[1][i - 1];
-	        
-	        dp[2][i] = !dp[2][i];
 	    }
-	    if(dp[1][N] == true) return 1;
+	    if(dp[0][N] == true) return 1;
 	    return 0;
 	}
 };
