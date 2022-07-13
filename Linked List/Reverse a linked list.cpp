6c8e42d9063712195b67155ad5732d719eeb9f2d
Harsh Gupta <you@example.com>
2022-07-13 08:21:12 -0700

    Solved

---
+++ Linked List/Reverse a linked list.cpp
@@ -0,0 +1,54 @@
+// ITERATIVE
+
+class Solution
+{
+    public:
+    //Function to reverse a linked list.
+    struct Node* reverseList(struct Node *head)
+    {
+        struct Node* p1 = head;
+        if(p1 -> next == NULL) return p1;
+        struct Node* p2 = p1 -> next;
+        p1 -> next = NULL;
+        struct Node* p3 = p2 -> next;
+        while(p3 != NULL){
+            p2 -> next = p1;
+            p1 = p2;
+            p2 = p3;
+            p3 = p2 -> next;
+        }
+        p2 -> next = p1;
+        return p2;
+    }
+    
+};
+
+// RECURSIVE
+
+class Solution
+{
+    public:
+    //Function to reverse a linked list.
+    
+    void Reverse(struct Node* &p1 , struct Node* &p2,struct Node* &p3){
+        if(p3 == NULL) return;
+        p2 -> next = p1;
+        p1 = p2;
+        p2 = p3;
+        p3 = p2 -> next;
+        Reverse(p1,p2,p3);
+    }
+    
+    struct Node* reverseList(struct Node *head)
+    {
+        struct Node* p1 = head;
+        if(p1 -> next == NULL) return p1;
+        struct Node* p2 = p1 -> next;
+        p1 -> next = NULL;
+        struct Node* p3 = p2 -> next;
+        Reverse(p1,p2,p3);
+        p2 -> next = p1;
+        return p2;
+    }
+    
+};
