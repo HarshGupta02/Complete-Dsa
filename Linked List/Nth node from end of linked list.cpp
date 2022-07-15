f951f798d709b1f069c6bcb5347d1557b04f3c6a
Harsh Gupta <you@example.com>
2022-07-14 22:23:10 -0700

    SOlved

---
+++ Linked List/Nth node from end of linked list.cpp
@@ -0,0 +1,24 @@
+/*
+here we use slow and fast pointer and we first move the fast pointer by n distance and then move slow and fast
+pointer together and this ensures that the gap will always be n and when fast pointer reaches end so the slow will
+be the ans as it will be n distance from behind.
+*/
+
+
+int getNthFromLast(Node *head, int n)
+{
+    struct Node* slow = head;
+    struct Node* fast = head;
+    int temp = n - 1;
+    while(temp > 0){
+        fast = fast -> next;
+        if(fast == NULL) break;
+        temp --;
+    }
+    if(temp != 0) return -1;
+    while(fast -> next != NULL){
+        slow = slow -> next;
+        fast = fast -> next;
+    }
+    return slow -> data;
+}
