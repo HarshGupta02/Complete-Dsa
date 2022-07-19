750ec74fcdc4fa675110ef6bf0db56e24c8d18f6
Harsh Gupta <you@example.com>
2022-07-19 09:23:06 -0700

    Solved

---
+++ Binary Trees/Postorder Traversal.cpp
@@ -0,0 +1,18 @@
+/// recursive :
+
+void f(Node* root , vector<int> &ans){
+    if(root == NULL) return;
+    f(root -> left,ans);
+    f(root -> right,ans);
+    ans.push_back(root -> data);
+}
+
+vector <int> postOrder(Node* root)
+{
+    vector<int>ans;
+    f(root,ans);
+    return ans;
+}
+
+/// iterative :
+
