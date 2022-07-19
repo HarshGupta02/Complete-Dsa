c85e46c04dca46716dc0127843854a6794206fef
Harsh Gupta <you@example.com>
2022-07-19 13:06:14 -0700

    Solved

--- Binary Trees/Left View of Binary Tree.cpp
+++ Binary Trees/Left View of Binary Tree.cpp
@@ -1,3 +1,5 @@
+// Approach 1 :
+
 vector<int> leftView(Node *root)
 {
     vector<int>ans;
@@ -16,5 +18,21 @@ vector<int> leftView(Node *root)
         if(parent -> left != NULL) q.push({parent -> left , par_level + 1});
         if(parent -> right != NULL) q.push({parent -> right , par_level + 1});
     }
+    return ans;
+}
+
+// Approach 2 : 
+
+void f(Node* root , int level , vector<int> &ans){
+    if(root == NULL) return ;
+    if(level == (int)ans.size()) ans.push_back(root -> data);
+    f(root -> left,level + 1, ans);
+    f(root -> right,level + 1, ans);
+}
+
+vector<int> leftView(Node *root)
+{
+    vector<int>ans;
+    f(root,0,ans);
     return ans;
 }
