92099005a0290cd54945531c0a77dd72740f9f76
Harsh Gupta <you@example.com>
2022-07-23 10:03:26 -0700

    Solved

--- Binary Search Trees/Minimum element in BST.cpp
+++ Binary Search Trees/Minimum element in BST.cpp
@@ -3,3 +3,9 @@ int minValue(Node* root) {
     if(root -> left == NULL) return root -> data;
     return minValue(root -> left);
 }
+
+int maxValue(Node* root) {
+    if(root == NULL) return -1;
+    if(root -> right == NULL) return root -> data;
+    return maxValue(root -> right);
+}
