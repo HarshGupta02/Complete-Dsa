9735056c1fa7787820d7102731f7305eb143dfc0
Harsh Gupta <you@example.com>
2022-07-25 14:51:20 -0700

    Morris Traversal added

--- Binary Trees/Preorder Traversal.cpp
+++ Binary Trees/Preorder Traversal.cpp
@@ -33,3 +33,31 @@ vector <int> preorder(Node* root)
     }
     return ans;
 }
+
+// MORRIS TRAVERSAL : TIME : O(N) , SPACE = O(1)
+
+vector <int> preorder(Node* root)
+{
+    vector<int>ans;
+    Node* curr = root;
+    while(curr != NULL){
+        if(curr -> left == NULL){
+            ans.push_back(curr -> data);
+            curr = curr -> right;
+        }else{
+            Node* prev = curr -> left;
+            while(prev -> right != NULL and prev -> right != curr){
+                prev = prev -> right;
+            }
+            if(prev -> right == NULL){
+                prev -> right = curr;
+                ans.push_back(curr -> data);
+                curr = curr -> left;
+            }else{
+                prev -> right = NULL;
+                curr = curr -> right;
+            }
+        }
+    }
+    return ans;
+}
