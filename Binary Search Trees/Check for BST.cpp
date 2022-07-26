ccae9d414556c5ab5cbed5f8acfe32aeefe73949
Harsh Gupta <you@example.com>
2022-07-26 15:07:18 -0700

    Optimised

--- Binary Search Trees/Check for BST.cpp
+++ Binary Search Trees/Check for BST.cpp
@@ -1,3 +1,5 @@
+// METHOD 1:
+
 class Solution
 {
     public:
@@ -31,6 +33,42 @@ class Solution
     {
         bool is = true;
         pair<int,int> x = f(root,is);
+        return is;
+    }
+};
+
+
+// METHOD 2:
+
+class Info{
+    public:
+    int mini , maxi;
+    
+    Info(int mini , int maxi){
+        this -> mini = mini;
+        this -> maxi = maxi;
+    }
+};
+
+class Solution
+{
+    public:
+    //Function to check whether a Binary Tree is BST or not.
+    
+    Info f(Node* root , bool &is){
+        if(root == NULL) return Info(INT_MAX , INT_MIN);
+        auto curr_left = f(root -> left,is);
+        auto curr_right = f(root -> right,is);
+        if(curr_left.maxi < root -> data and root -> data < curr_right.mini){
+            return Info(min(curr_left.mini , root -> data) , max(curr_right.maxi , root -> data));
+        }
+        is = false; return curr_left;
+    }
+    
+    bool isBST(Node* root) 
+    {
+        bool is = true;
+        Info temp = f(root,is);
         return is;
     }
 };
