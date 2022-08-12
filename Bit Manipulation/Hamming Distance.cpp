1f10f18855ca1c188477918eb4f9778e748358d8
Harsh Gupta <you@example.com>
2022-08-11 18:07:42 -0700

    Solved

---
+++ Bit Manipulation/Hamming Distance.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int hammingDistance(int x, int y) {
+        int cnt = 0;
+        while(x != 0 or y != 0){
+            if((x & 1) != (y & 1)) cnt ++;
+            x = x >> 1; y = y >> 1;
+        }
+        return cnt;
+    }
+};
