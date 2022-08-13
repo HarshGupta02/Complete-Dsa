8624b8dfa831abdee54f55130f304dd000c57a69
Harsh Gupta <you@example.com>
2022-08-13 10:20:32 -0700

    Solved

--- Dymanic_Programming/Maximum length chain.cpp
+++ Dymanic_Programming/Maximum length chain.cpp
@@ -1,3 +1,5 @@
+/// TIME = O(N ^ 2), SPACE = O(N)
+
 bool cmp(const val &a , const val &b){
     return a.first < b.first;
 }
@@ -15,3 +17,29 @@ int maxChainLen(struct val p[],int n)
     }
     return *max_element(dp.begin(),dp.end());
 }
+
+
+// TIME = O(N * LOGN), SPACE = O(1)
+
+class Solution{
+public:
+    /*You are required to complete this method*/
+    
+    static bool cmp(const val &a, const val &b){
+        return a.second < b.second;
+    }
+    
+    int maxChainLen(struct val p[],int n){
+        sort(p, p + n, cmp);
+        int ans = 1;
+        int glob_start = p[0].first, glob_end = p[0].second;
+        for(int i = 1; i < n ; i ++){
+            if(p[i].first > glob_end){
+                ans ++;
+                glob_start = p[i].first;
+                glob_end = p[i].second;
+            }
+        }
+        return ans;
+    }
+};
