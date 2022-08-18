bc283ab481b439db962dcf9f9c550f17f26e1f39
Harsh Gupta <you@example.com>
2022-08-18 13:27:26 -0700

    Solved

--- Arrays/Minimum Moves to Equal Array Elements.cpp
+++ Arrays/Minimum Moves to Equal Array Elements.cpp
@@ -1,3 +1,25 @@
+/*
+
+[a,b,c,d...] => [x,x,x,x....]
+
+sum = a + b + c + d ...
+
+in 1 move , total sum of all elements increase by n - 1.
+
+so after m moves, total = sum + m * (n - 1) = x * n
+
+=> sum + m * (n - 1) = x * n
+
+and x = min_num + m
+
+2 -> 3 -> 4 -> 5 -> 6
+
+sum + m * n - m = (min_num + m) * n
+
+=> m = sum - (n * min_num);
+
+*/
+
 class Solution {
 public:
     int minMoves(vector<int>& nums) {
