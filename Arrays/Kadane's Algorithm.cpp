2fe3837e232109e6408fe6cc91210fb46262d35a
Harsh Gupta <you@example.com>
2022-10-12 11:26:17 +0530

    Solved

--- Arrays/Kadane's Algorithm.cpp
+++ Arrays/Kadane's Algorithm.cpp
@@ -6,11 +6,21 @@ class Solution{
     //Function to find the sum of contiguous subarray with maximum sum.
     long long maxSubarraySum(int arr[], int n){
         ll curr_sum = 0 , glob_sum = -1e18;
+        ll s = 0, st = 0, en = 0;
         for(int i = 0; i < n ; i ++){
             curr_sum += arr[i];
-            glob_sum = max(glob_sum , curr_sum);
-            if(curr_sum < 0) curr_sum = 0;
+            if(curr_sum > glob_sum){
+                glob_sum = curr_sum;
+                st = s; en = i;
+            }
+            if(curr_sum < 0){
+                curr_sum = 0;
+                s = i + 1;
+            }
         }
+        cout << "the subarray with max subarray sum is " << endl;
+        for(int i = st; i <= en; i ++) cout << arr[i] << " ";
+        cout << endl;
         return glob_sum;
     }
 };
