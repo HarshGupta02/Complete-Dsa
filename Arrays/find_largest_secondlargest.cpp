ecc0de38939c7a777b053fce2bba5806875b862a
Harsh Gupta <simpcoder02@gmail.com>
2023-01-13 09:46:57 +0530

    Solved

---
+++ Arrays/find_largest_secondlargest.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+const ll MOD = 1e9+7;
+
+#define nline "\n"
+#define all(a) (a).begin(),(a).end()
+#define fast_cin() ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+
+void solve(){
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for(int i = 0; i < n; i ++) cin >> a[i];
+    int m = 0, sm = 0;
+    for(int i = 0; i < n; i ++){
+        if(a[i] > sm) sm = a[i];
+        if(sm > m) swap(m, sm);
+    }
+    cout << m << " " << sm << nline;
+}
+
+int main() {
+    fast_cin();
+    ll test_cases = 1;
+    // cin >> test_cases;
+    for(ll it=1;it<=test_cases;it++) {
+        solve();
+    }
+    return 0;
+}
