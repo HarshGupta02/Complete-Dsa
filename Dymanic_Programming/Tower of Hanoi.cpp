62a6fc339386d217abad468212aae04776308adf
Harsh Gupta <simpcoder02@gmail.com>
2023-02-01 11:59:14 +0530

    Solved

---
+++ Dymanic_Programming/Tower of Hanoi.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define endl "\n"
+using ll = long long int;
+
+ll fast_exp(ll a, ll b){
+    ll res = 1;
+    while(b > 0){
+        if(b & 1) res = (res * a);
+        b >>= 1;
+        a = (a * a);
+    }
+    return res;
+}
+
+void f(int n, int a, int b, int c){
+    if(n == 1) {
+        cout << a << " " << c << "\n";
+        return;
+    }
+    f(n - 1, a, c, b);
+    cout << a << " " << c << "\n";
+    f(n - 1, b, a, c);
+}
+
+int main(){
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    ll n;
+    cin >> n;
+    cout << fast_exp(2, n) - 1 << "\n";
+    f(n, 1, 2, 3);
+    return 0;
+}
