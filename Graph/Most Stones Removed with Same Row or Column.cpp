6eacaad6b203a5cee908a23e97275014e66a31d6
Harsh Gupta <simpcoder02@gmail.com>
2023-06-08 00:23:55 +0530

    Solved

--- Graph/Most Stones Removed with Same Row or Column.cpp
+++ Graph/Most Stones Removed with Same Row or Column.cpp
@@ -86,3 +86,61 @@ public:
         return n - s.size();
     }
 };
+
+// SOLUTION 3: 
+
+/*
+SO HERE WE BASICALLY TREAT EACH ROW AND EACH COL AS A SEPARATE NODE AND JUST
+CHANGE THE INDICES OF THE COLUMN NODES AND FOR EACH STONE, WE KEEP ON MERGING
+THE CORRESPONDING ROW_NODE AND COL_NODES.
+
+*/
+
+class DSU{
+public:
+    vector<int> parent, rank;
+    DSU(int n) {
+        parent.resize(n); rank.resize(n, 1);
+        for(int i = 0; i < n; i ++) parent[i] = i;
+    }
+    
+    int get_parent(int a) {
+        return (parent[a] == a ? a : get_parent(parent[a]));
+    }
+    
+    void mergee(int a, int b) {
+        a = get_parent(a);
+        b = get_parent(b);
+        if(rank[b] < rank[a]) parent[b] = a;
+        else if(rank[a] < rank[b]) parent[a] = b;
+        else {
+            rank[a] ++;
+            parent[b] = a;
+        }
+    }
+};
+
+class Solution {
+public:
+    int removeStones(vector<vector<int>>& stones) {
+        int n = stones.size();
+        int max_row = 0, max_col = 0;
+        for(int i = 0; i < n; i ++) {
+            max_row = max(max_row, stones[i][0]);
+            max_col = max(max_col, stones[i][1]);
+        }
+        DSU dsu(max_row + max_col + 2);
+        unordered_map<int, int> mp;
+        for(auto it : stones) {
+            int node_row = it[0], node_col = max_row + 1 + it[1];
+            mp[node_row] = 1; mp[node_col] = 1;
+            if(dsu.get_parent(node_row) != dsu.get_parent(node_col))
+                dsu.mergee(node_row, node_col);
+        }
+        set<int> st;
+        for(auto it : mp) {
+            st.insert(dsu.get_parent(it.first));
+        }
+        return n - (int)st.size();
+    }
+};
