1def1780e1d52c9abafb46ac822e313c8a8362e4
Harsh Gupta <simpcoder02@gmail.com>
2023-06-12 11:11:01 +0530

    Solved

--- Matrix/Rotate Image.cpp
+++ Matrix/Rotate Image.cpp
@@ -1,3 +1,11 @@
+/*
+to rotate right, we see that each column is being  converted to a row and vice versa 
+and so we first take the transpose of the given matrix and then we reverse each row.
+
+to rotate left, first do reversing each row then do transpose
+
+*/
+
 class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
