58a8cf62eeb543b959d60a520eeada8d39efbcca
Harsh Gupta <simpcoder02@gmail.com>
2023-07-04 16:03:18 +0530

    Solved

--- Arrays/Binary Subarrays With Sum.cpp
+++ Arrays/Binary Subarrays With Sum.cpp
@@ -1,3 +1,5 @@
+// METHOD1:
+
 class Solution {
 public:
     int numSubarraysWithSum(vector<int>& nums, int goal) {
@@ -17,5 +19,58 @@ public:
             }else i ++;
         }
         return ans;
+    }
+};
+
+// METHOD 2: COUNT NO OF SUBARRAY WITH SUM K
+
+class Solution {
+public:
+    int numSubarraysWithSum(vector<int>& nums, int goal) {
+        int n = nums.size();
+        unordered_map<int, int> mp;
+        mp[0] = 1;
+        int sum = 0, ans = 0;
+        for(int i = 0; i < n; i ++) {
+            sum += nums[i];
+            if(mp.find(sum - goal) != mp.end()) ans += mp[sum - goal];
+            mp[sum] ++;
+        }
+        return ans;
+    }
+};
+
+// METHOD 3:
+
+/*
+in this method, to find no of subarray with sum k, we find no of subarray with sum atmost k 
+minus the no of subarray with sum atmost k - 1. the result is no of subarray with sum exactly
+k. 
+for atmost k as sum, if sum < goal so we add all the no of new subarray that the head comes
+at the end.
+*/
+
+class Solution {
+public:
+    
+    int atMost(int n, vector<int> &nums, int goal) {
+        if(goal < 0) return 0;
+        int sum = 0, ans = 0;
+        int i = 0, j = 0;
+        while(i < n and j < n) {
+            sum += nums[j];
+            while(sum > goal) {
+                sum -= nums[i];
+                i ++;
+            }
+            ans += (j - i) + 1;
+            j ++;
+        }
+        return ans;
+    }
+    
+    int numSubarraysWithSum(vector<int>& nums, int goal) {
+        int n = nums.size();
+        return atMost(n, nums, goal) - atMost(n, nums, goal - 1);
     }
 };
