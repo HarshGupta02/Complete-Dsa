ee839342570912d7b96cfabe2c7b0fe3ff7c8308
Harsh Gupta <simpcoder02@gmail.com>
2023-07-06 22:08:37 +0530

    Solved

---
+++ Binary Search Trees/Two Sum IV - Input is a BST.cpp
@@ -0,0 +1,48 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class BSTIterator{
+private:
+    stack<TreeNode*> stk;
+    bool reverse = false;
+    void push(TreeNode* root) {
+        while(root != NULL) {
+            stk.push(root);
+            root = (!reverse ? root -> left : root -> right);
+        }
+    }
+public:
+    BSTIterator(TreeNode* root, bool reverse = false) {
+        this -> reverse = reverse;
+        push(root);
+    }
+    
+    int next() {
+        TreeNode* node = stk.top(); stk.pop();
+        push(!reverse ? node -> right : node -> left);
+        return node -> val;
+    }
+};
+
+class Solution {
+public:
+    bool findTarget(TreeNode* root, int k) {
+        BSTIterator leftItr(root), rightItr(root, true);
+        int left = leftItr.next(), right = rightItr.next();
+        while(left < right) {
+            if(left + right == k) return true;
+            if(left + right < k) left = leftItr.next();
+            else right = rightItr.next();
+        }
+        return false;
+    }
+};
