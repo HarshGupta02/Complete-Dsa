c69b0dd254879c39d99c67047f1b97133862d170
Harsh Gupta <simpcoder02@gmail.com>
2023-07-08 18:49:41 +0530

    Solved

--- Arrays/Candy.cpp
+++ Arrays/Candy.cpp
@@ -16,3 +16,35 @@ public:
         return sum;
     }
 };
+
+// TIME = O(N), SPACE = O(1)
+
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        int n = ratings.size();
+        int i = 1, candy = n;
+        while(i < n) {
+            if(ratings[i] == ratings[i - 1]) {
+                i ++; continue;
+            }
+            // increasing peak
+            int peaks = 0;
+            while(i < n and ratings[i] > ratings[i - 1]) {
+                peaks ++;
+                candy += peaks;
+                i ++;
+            }
+            // decreasing peak or increasing valley
+            int valley = 0;
+            while(i < n and ratings[i] < ratings[i - 1]) {
+                valley ++;
+                candy += valley;
+                i ++;
+            }
+            // since candy included both peak and valley so needed only max of them so removed min of them
+            candy -= min(peaks, valley);
+        }
+        return candy;
+    }
+};
