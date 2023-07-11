a98fba5652c61e3b774af9855c5cf7e310d47134
Harsh Gupta <simpcoder02@gmail.com>
2023-07-11 17:19:04 +0530

    Solved

--- Linked List/Merge K sorted linked lists.cpp
+++ Linked List/Merge K sorted linked lists.cpp
@@ -50,3 +50,42 @@ public:
         return curr;
     }
 };
+
+// METHOD 2: TIME = O(N * LOG(K)), SPACE = O(K)
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+class cmp{
+public:
+    bool operator()(ListNode* a, ListNode* b) {
+        return a -> val > b -> val;
+    }
+};
+
+class Solution {
+public:
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        int k = lists.size();
+        priority_queue<ListNode*, vector<ListNode*>, cmp> pq;
+        for(int i = 0; i < k; i ++) {
+            if(lists[i] != NULL) pq.push(lists[i]);
+        }
+        ListNode* head = new ListNode(-1);
+        ListNode* curr = head;
+        while(!pq.empty()) {
+            ListNode* node = pq.top(); pq.pop();
+            if(node -> next != NULL) pq.push(node -> next);
+            curr -> next = node; curr = curr -> next;
+        }
+        return head -> next;
+    }
+};
